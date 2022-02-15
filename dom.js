8aecf21fe9d2139f0d686d9e5a49a2f361523b19
andhikarei <andhikareihan349@gmail.com>
2022-02-15 17:17:53 +0700

    Add draw polygon

--- dom.js
+++ dom.js
@@ -12,7 +12,7 @@ let drawObject = '';
 let isDrawing = false;
 
 // Drawing attribute initialization.
-let x1,x2,x3,y1,y2,y3;
+let x1,x2,y1,y2;
 let vertices = [];
 let drawType = gl.TRIANGLES;
 let rgbVal = [0, 0, 0];
@@ -22,21 +22,51 @@ const btnDrawLine = document.getElementById("draw-line");
 const btnDrawSquare = document.getElementById("draw-square");
 const btnDrawPolygon = document.getElementById("draw-polygon");
 const btnDrawRectangle = document.getElementById("draw-rectangle");
+const btnEndPolygon = document.getElementById("end-polygon");
 
+// Button event handler.
 // Change the drawn object based on button.
 btnDrawLine.addEventListener("click", () => {
-    drawObject = "line";
+    if (drawObject == '') {
+        drawObject = 'line';
+    };
 });
 btnDrawSquare.addEventListener("click", () => {
-    drawObject = "square";
+    if (drawObject == '') {
+        drawObject = 'square';
+    };
 });
 btnDrawPolygon.addEventListener("click", () => {
-    drawObject = "polygon";
+    if (drawObject == '') {
+        drawObject = 'polygon';
+    };
 });
 btnDrawRectangle.addEventListener("click", () => {
-    drawObject = "rectangle";
+    if (drawObject == '') {
+        drawObject = 'rectangle';
+    };
 });
 
+// End drawing polygon.
+btnEndPolygon.addEventListener("click", () => {
+    if (drawObject == 'polygon' && vertices.length > 4) {
+        // Remove last point
+        vertices.pop();
+        vertices.pop();
+
+        // Create Square object and store it in array.
+        const polygon = new Shape(idx, drawType, vertices, rgbVal, 'polygon');
+        allShapes.push(polygon);
+
+        // Refresh drawing attribute.
+        vertices = [];
+        idx = idx + 1;
+        drawObject = '';
+        
+        // Refresh screen.
+        renderAll();
+    }
+});
 
 // Mouse event handler on canvas.
 // Mouse click event handler.
@@ -56,6 +86,11 @@ const mouseDown = function(e){
         if ((drawObject == "rectangle" || drawObject == "square") && vertices.length == 8) {
             isDrawing = false;
         }
+
+        // Initiate drawing Polygon.
+        if (drawObject == "polygon" && vertices.length >= 4) {
+            vertices.push(canvasCoordinateX(x2), canvasCoordinateY(y2));
+        }
         
     }
 
@@ -68,7 +103,8 @@ const mouseDown = function(e){
 const mouseUp = function(e) {
     // Already finish drawing object.
     if (drawObject != '' && !isDrawing){
-        // Determine draw object.
+
+        // Determine draw, reate and save object.
         if (drawObject == 'line') {
             // Create Line object and store it in array.
             const line = new Shape(idx, gl.LINES, vertices, rgbVal, 'line');
@@ -103,13 +139,14 @@ const mouseMove = function(e) {
     // Currently drawing object.
     if (isDrawing){
         // Capture mouse coordinate.
-        const x2 = e.pageX;
-        const y2 = e.pageY;
+        let x2 = e.pageX;
+        let y2 = e.pageY;
         
         // Draw temporary shape for animation.
         // Don't need to store object in array.
         // Draw temporary line.
         if (drawObject == "line") {
+            // Line draw attribute.
             drawType = gl.LINES
             vertices = [
                 canvasCoordinateX(x1), canvasCoordinateY(y1), 
@@ -119,21 +156,28 @@ const mouseMove = function(e) {
         
         // Draw temporary square.
         if (drawObject == "square") {
-            drawType = gl.TRIANGLE_STRIP;
+            // Count longest distance between x and y.
+            const distance = Math.abs(x1 - x2) > Math.abs(y1 - y2) ? Math.abs(x1 - x2) 
+                : Math.abs(y1 - y2);
 
-            const distance = Math.abs(x1 - x2) > Math.abs(y1 - y2) ? x1 - x2 
-                : y1 - y2
+            // Update x2 and y2 depending on distance and position relative to x1.
+            x2 = x1 > x2 ? x1-distance: x1 + distance
+            y2 = y1 > y2 ? y1-distance: y1 + distance
+            
+            // Square draw attribute.
+            drawType = gl.TRIANGLE_STRIP;
             vertices = [
                 canvasCoordinateX(x1), canvasCoordinateY(y1),
-                canvasCoordinateX(x1), canvasCoordinateY(y1-distance),
-                canvasCoordinateX(x1-distance), canvasCoordinateY(y1),
-                canvasCoordinateX(x1-distance), canvasCoordinateY(y1-distance),
+                canvasCoordinateX(x1), canvasCoordinateY(y2),
+                canvasCoordinateX(x2), canvasCoordinateY(y1),
+                canvasCoordinateX(x2), canvasCoordinateY(y2),
             ];
 
         }
         
         // Draw temporary rectangle.
         if (drawObject == "rectangle") {
+            // Rectangle draw attribute.
             drawType = gl.TRIANGLE_FAN;
             vertices = [
                 canvasCoordinateX(x1), canvasCoordinateY(y1),
@@ -141,6 +185,28 @@ const mouseMove = function(e) {
                 canvasCoordinateX(x2), canvasCoordinateY(y2),
                 canvasCoordinateX(x2), canvasCoordinateY(y1),
             ];
+        }
+
+        // Draw temporary polygon.
+        if (drawObject == "polygon") {
+            // Temporary draw line when polygon side <= 1.
+            if (vertices.length == 0 || vertices.length == 4) {
+                drawType = gl.LINES;
+                vertices = [
+                    canvasCoordinateX(x1), canvasCoordinateY(y1), 
+                    canvasCoordinateX(x2), canvasCoordinateY(y2)
+                ];
+            } else {
+                drawType = gl.TRIANGLE_FAN;
+                // Remove last point.
+                vertices.pop();
+                vertices.pop();
+
+                // Add the latest coordinate.
+                vertices.push(canvasCoordinateX(x2), canvasCoordinateY(y2));
+            }
+            
+            // Polygon draw attribute.
         }
 
         // Render temporary object.
